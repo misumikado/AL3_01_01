78903b041e8020791a84d3d59bf85f5cb2b735f0
MSI\k021g <k021g1161@g.neec.ac.jp>
2022-04-14 15:49:07 +0900

    01-02

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,9 +4,16 @@
 
 using namespace DirectX;
 
-GameScene::GameScene() {}
+GameScene::GameScene() {
 
-GameScene::~GameScene() {}
+}
+
+GameScene::~GameScene() { 
+	
+	delete sprite_;
+	delete model_;
+
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +21,61 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	//ファイルを指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	//スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+   //トランスフォームの初期化
+	worldTransform_.Initialize();
+	
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	//サウンドデータの読み込み
+	soundDataHandle_=audio_->LoadWave("se_sad03.wav");
+
+	//音声再生
+	audio_->PlayWave(soundDataHandle_);
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+
+	//スプライトの今の座標を取得
+	XMFLOAT2 position = sprite_->GetPosition();
+	//座標を{2,0}移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	//移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+
+	//スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		//音声停止
+		audio_->StopWave(voiceHandle_);
+	}
+
+	//デバッグテキストの表示
+	debugText_->Print("Kaizoku ni oreha naru", 50, 50, 1.0f);
+
+	//書式指定付き表示
+	debugText_->SetPos(50, 70);
+	debugText_->Printf("year*%d", 2001);
+
+	//変数の値をインクリメント
+	value_++;
+	//値を含んだ文字列
+	std::string strDebug = std::string("Value") + std::to_string(value_);
+	//デバッグテキストの表示
+	debugText_->Print(strDebug, 50, 100, 1.0f);
+}
 
 void GameScene::Draw() {
 
@@ -29,6 +88,8 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
+
+
 	/// </summary>
 
 	// スプライト描画後処理
@@ -44,7 +105,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	//3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -56,6 +118,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
